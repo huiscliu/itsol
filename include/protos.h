38640dd0c8626fffdc8fce2f4350596056297b76
huiscliu <username@users.noreply.github.com>
2019-02-01 21:05:59 -0700

    remove extern

--- include/protos.h
+++ include/protos.h
@@ -68,16 +68,16 @@
 #define MAX_MAT			100
 
 /* FORTRAN routines */
-extern void readmtc(int*,  int*,  int*,  char*,  double*,  int*,
+void readmtc(int*,  int*,  int*,  char*,  double*,  int*,
 	     int*,  double*, int*,  char*,  int*,  int*,  int*,
 	     char*,  char*, char*,  int*) ;
-extern void csrcsc (int*, int*, int*, double*, int*, int*, double*,
+void csrcsc (int*, int*, int*, double*, int*, int*, double*,
 		    int*, int*) ; 
-extern void qsplit(double *a, int *ind, int *n, int *ncut);	
-extern void dgesvd(char*, char*, int*, int*, double*, int*, double*,
+void qsplit(double *a, int *ind, int *n, int *ncut);	
+void dgesvd(char*, char*, int*, int*, double*, int*, double*,
 		   double *, int*, double*, int*, double*, int*,
 		   int*); 
-extern void csrcoo ( int *, int *, int *, double *, int *, int *, int *,
+void csrcoo ( int *, int *, int *, double *, int *, int *, int *,
 		double *, int *, int *, int *);    
 
 #if defined(_IBM)
@@ -122,197 +122,197 @@ extern void csrcoo ( int *, int *, int *, double *, int *, int *, int *,
 #define DGETRI(n, a, lda, ipvt, work, lwork, info)			\
   dgetri(&(n), (a), &(lda), (ipvt), (work), &(lwork), (info))
 
-extern double ddot(int *n, double *x, int *incx, double *y, int
+double ddot(int *n, double *x, int *incx, double *y, int
 		    *incy);  
-extern void   dcopy(int *n, double *x, int *incx, double *y, int
+void   dcopy(int *n, double *x, int *incx, double *y, int
 		    *incy); 
-extern void   dscal(int *n, double *alpha, double *x, int *incx);
-extern void   daxpy(int *n, double *alpha, double *x, int *incx,
+void   dscal(int *n, double *alpha, double *x, int *incx);
+void   daxpy(int *n, double *alpha, double *x, int *incx,
 		    double *y, int *incy);
-extern double dnrm2(int *n, double *x, int *incx);
-extern void   idmin(int *n, double *sx, int *incx);
-extern void   dgemv(char *transa, int *m, int *n, double *alpha,
+double dnrm2(int *n, double *x, int *incx);
+void   idmin(int *n, double *sx, int *incx);
+void   dgemv(char *transa, int *m, int *n, double *alpha,
 		    double *a, int *lda, double *x, int *incx, double
 		    *beta, double *y, int *incy);
-extern void   dgemm(char *transa, char *transb, int *l, int *m, int
+void   dgemm(char *transa, char *transb, int *l, int *m, int
 		    *n, double *alpha, double *a, int *lda, double
 		    *b, int *ldb, double *beta, double *c, int *ldc);       
-extern void   dgetrf(int *m, int *n, double *a, int *lda, int *ipvt,
+void   dgetrf(int *m, int *n, double *a, int *lda, int *ipvt,
 		     int *info); 
-extern void   dgetri(int *n, double *a, int *lda, int *ipvt, double
+void   dgetri(int *n, double *a, int *lda, int *ipvt, double
 		     *work, int *lwork, int *info);
 #endif 
 
 /* sets.c */
-extern int nnz_arms (arms PreSt,  FILE *ft);
-extern void errexit(char *f_str, ...);
-extern void *Malloc(int nbytes, char *msg); 
-extern int setupCS(csptr amat, int len, int job); 
-extern int cleanCS(csptr amat);
-extern int nnz_cs (csptr A) ;
-extern int cscpy(csptr amat, csptr bmat);
-extern int setupP4 (p4ptr amat, int Bn, int Cn,  csptr F,  csptr E);
-extern int setupVBMat(vbsptr vbmat, int n, int *nB);
-extern int setupILUT(ilutptr amat, int len);
-extern int cleanVBMat(vbsptr vbmat); 
-extern int nnzVBMat(vbsptr vbmat) ;
-extern int memVBMat(vbsptr vbmat); 
-extern int setupVBILU(vbiluptr lu, int n, int *bsz);
-extern int cleanVBILU(vbiluptr lu); 
-extern int cleanILU( iluptr lu );
-extern int cleanILUT(ilutptr amat, int indic);
-extern int cleanP4(p4ptr amat);
-extern int mallocVBRow(vbiluptr lu, int nrow); 
-extern int mallocRow( iluptr lu, int nrow );
-extern void zrmC(int m, int n, BData data); 
-extern void copyBData(int m, int n, BData dst, BData src, int isig); 
-extern int CSRcs(int n, double *a, int *ja, int *ia, csptr mat, int 
+int nnz_arms (arms PreSt,  FILE *ft);
+void errexit(char *f_str, ...);
+void *Malloc(int nbytes, char *msg); 
+int setupCS(csptr amat, int len, int job); 
+int cleanCS(csptr amat);
+int nnz_cs (csptr A) ;
+int cscpy(csptr amat, csptr bmat);
+int setupP4 (p4ptr amat, int Bn, int Cn,  csptr F,  csptr E);
+int setupVBMat(vbsptr vbmat, int n, int *nB);
+int setupILUT(ilutptr amat, int len);
+int cleanVBMat(vbsptr vbmat); 
+int nnzVBMat(vbsptr vbmat) ;
+int memVBMat(vbsptr vbmat); 
+int setupVBILU(vbiluptr lu, int n, int *bsz);
+int cleanVBILU(vbiluptr lu); 
+int cleanILU( iluptr lu );
+int cleanILUT(ilutptr amat, int indic);
+int cleanP4(p4ptr amat);
+int mallocVBRow(vbiluptr lu, int nrow); 
+int mallocRow( iluptr lu, int nrow );
+void zrmC(int m, int n, BData data); 
+void copyBData(int m, int n, BData dst, BData src, int isig); 
+int CSRcs(int n, double *a, int *ja, int *ia, csptr mat, int 
 		 rsa); 
-extern int csrvbsrC(int job, int nBlk, int *nB, csptr csmat, vbsptr
+int csrvbsrC(int job, int nBlk, int *nB, csptr csmat, vbsptr
 		    vbmat);  
-extern int col2vbcol( int col, vbsptr vbmat );
-extern int nnz_vbilu(vbiluptr lu); 
-extern int nnz_lev4(p4ptr levmat, int *lev, FILE *ft);
-extern int setupILU( iluptr lu, int n );
-extern int CS2lum( int n, csptr Amat, iluptr mat, int typ);
-extern int COOcs(int n, int nnz,  double *a, int *ja, int *ia, csptr bmat);
+int col2vbcol( int col, vbsptr vbmat );
+int nnz_vbilu(vbiluptr lu); 
+int nnz_lev4(p4ptr levmat, int *lev, FILE *ft);
+int setupILU( iluptr lu, int n );
+int CS2lum( int n, csptr Amat, iluptr mat, int typ);
+int COOcs(int n, int nnz,  double *a, int *ja, int *ia, csptr bmat);
 void coocsr_(int*, int*, double*, int*, int*, double*, int*, int*);
 
 /* MatOps.c */
 void matvecC(csptr mat, double *x, double *y );
 void matvecCSC(SMatptr mat, double *x, double *y);
 int CondestC(iluptr lu, FILE *fp );
-extern int diag_scal(vbsptr vbmat);
-extern int diagvec(vbsptr vbmat, BData x, BData y);
-extern void matvec(csptr mata, double *x, double *y); 
-extern void matvecCSR(SMatptr mat, double *x, double *y);
-extern void matvecz(csptr mata, double *x, double *y, double *z);
-extern void vbmatvec(vbsptr vbmat, double *x, double *y);
-extern void luinv(int n, double *a, double *x, double *y); 
-extern int vblusolC(double *y, double *x, vbiluptr lu); 
-extern int lusolC( double *y, double *x, iluptr lu ); 
-extern int rpermC(csptr mat, int *perm); 
-extern int cpermC(csptr mat, int *perm) ; 
-extern int dpermC(csptr mat, int *perm) ; 
-extern int CSparTran(csptr amat, csptr bmat, CompressType *compress);
-extern double vbnorm2(int sz, double *a);
-extern void Lsol(csptr mata, double *b, double *x);
-extern void Usol(csptr mata, double *b, double *x);
-extern int ascend (p4ptr levmat, double *x, double *wk);
-extern int descend(p4ptr levmat, double *x, double *wk);
-extern int armsol2(double *x,  arms Prec);
-extern int condestArms(arms armspre, double *y, FILE *fp );
-extern int VBcondestC( vbiluptr, FILE *fp ); 
-extern int CondestLUM(iluptr lu, double *y, double *x, FILE *fp );
-extern void matvecVBR(SMatptr mat, double *x, double *y);
-extern void matvecLDU(SMatptr mat, double *x, double *y);
-extern int  preconILU(double *x, double *y, SPreptr mat);
-extern int  preconVBR(double *x, double *y, SPreptr mat);
-extern int  preconLDU(double *x, double *y, SPreptr mat);
-extern int  preconARMS(double *x, double *y, SPreptr mat);
-extern p4ptr Lvsol2(double *x, int nlev, p4ptr levmat, ilutptr ilusch) ;
-extern int   Uvsol2(double *x, int nlev, int n, p4ptr levmat, ilutptr
+int diag_scal(vbsptr vbmat);
+int diagvec(vbsptr vbmat, BData x, BData y);
+void matvec(csptr mata, double *x, double *y); 
+void matvecCSR(SMatptr mat, double *x, double *y);
+void matvecz(csptr mata, double *x, double *y, double *z);
+void vbmatvec(vbsptr vbmat, double *x, double *y);
+void luinv(int n, double *a, double *x, double *y); 
+int vblusolC(double *y, double *x, vbiluptr lu); 
+int lusolC( double *y, double *x, iluptr lu ); 
+int rpermC(csptr mat, int *perm); 
+int cpermC(csptr mat, int *perm) ; 
+int dpermC(csptr mat, int *perm) ; 
+int CSparTran(csptr amat, csptr bmat, CompressType *compress);
+double vbnorm2(int sz, double *a);
+void Lsol(csptr mata, double *b, double *x);
+void Usol(csptr mata, double *b, double *x);
+int ascend (p4ptr levmat, double *x, double *wk);
+int descend(p4ptr levmat, double *x, double *wk);
+int armsol2(double *x,  arms Prec);
+int condestArms(arms armspre, double *y, FILE *fp );
+int VBcondestC( vbiluptr, FILE *fp ); 
+int CondestLUM(iluptr lu, double *y, double *x, FILE *fp );
+void matvecVBR(SMatptr mat, double *x, double *y);
+void matvecLDU(SMatptr mat, double *x, double *y);
+int  preconILU(double *x, double *y, SPreptr mat);
+int  preconVBR(double *x, double *y, SPreptr mat);
+int  preconLDU(double *x, double *y, SPreptr mat);
+int  preconARMS(double *x, double *y, SPreptr mat);
+p4ptr Lvsol2(double *x, int nlev, p4ptr levmat, ilutptr ilusch) ;
+int   Uvsol2(double *x, int nlev, int n, p4ptr levmat, ilutptr
 		    ilusch); 
-extern void  SchLsol(ilutptr ilusch, double *y) ;
-extern void  SchUsol(ilutptr ilusch, double *y) ;
+void  SchLsol(ilutptr ilusch, double *y) ;
+void  SchUsol(ilutptr ilusch, double *y) ;
 #ifndef _IBM
-extern void dgemv(char*, int *, int*, double*, double *, int*,
+void dgemv(char*, int *, int*, double*, double *, int*,
 		  double*, int*, double*, double*, int*);  
-extern void dgemm(char*, char*, int*, int*, int*, double*, double*,
+void dgemm(char*, char*, int*, int*, int*, double*, double*,
 		  int*, double*, int*, double*, double*, int*) ;  
-extern void dgetrf(int*, int*, double*, int*, int*, int*); 
-extern void dgetri(int*, double*, int*, int*, double*,  int*, int* );
-extern double dnrm2( int *, double *, int * );
-extern void dscal(int*, double*, double*, int*); 
+void dgetrf(int*, int*, double*, int*, int*, int*); 
+void dgetri(int*, double*, int*, int*, double*,  int*, int* );
+double dnrm2( int *, double *, int * );
+void dscal(int*, double*, double*, int*); 
 #endif 
-extern int invGauss(int nn, double *A); 
-extern int invSVD(int nn, double *A) ;
+int invGauss(int nn, double *A); 
+int invSVD(int nn, double *A) ;
 
 /* setblks.c */ 
-extern int KeyComp(const void *vfst, const void *vsnd);
-extern int init_blocks(csptr, int *, int **, int **, double, double *,
+int KeyComp(const void *vfst, const void *vsnd);
+int init_blocks(csptr, int *, int **, int **, double, double *,
 		       double *);  
 
 /* upper directory */
-extern int vbilukC( int lofM, vbsptr vbmat, vbiluptr lu, FILE *fp ); 
-extern int vbilutC( vbsptr vbmat, vbiluptr lu, int lfil, double tol,
+int vbilukC( int lofM, vbsptr vbmat, vbiluptr lu, FILE *fp ); 
+int vbilutC( vbsptr vbmat, vbiluptr lu, int lfil, double tol,
 		    BData *w, FILE *fp ); 
-extern int ilutc(iluptr mt, iluptr lu, int lfil, double tol, int drop,
+int ilutc(iluptr mt, iluptr lu, int lfil, double tol, int drop,
 		 FILE *fp ); 
-extern int ilukC( int lofM, csptr csmat, iluptr lu, FILE *fp );
-extern int ilut( csptr csmat, iluptr lu, int lfil, double tol,
+int ilukC( int lofM, csptr csmat, iluptr lu, FILE *fp );
+int ilut( csptr csmat, iluptr lu, int lfil, double tol,
 		 FILE *fp );
-extern   int fgmr(SMatptr Amat, SPreptr lu, double *rhs, double *sol, double tol,
+  int fgmr(SMatptr Amat, SPreptr lu, double *rhs, double *sol, double tol,
 	   int im, int *itmax, FILE *fits ); 
-extern int arms2(csptr Amat, int *ipar, double *droptol, int *lfil, 
+int arms2(csptr Amat, int *ipar, double *droptol, int *lfil, 
 	  double tolind, arms PreMat, FILE *ft) ;
-extern int condestLU( iluptr, FILE *);
-extern int nnz_ilu( iluptr lu ); 
-extern void roscal (int* nrow, int* job, int* nrm, double *a, int *ja,
+int condestLU( iluptr, FILE *);
+int nnz_ilu( iluptr lu ); 
+void roscal (int* nrow, int* job, int* nrm, double *a, int *ja,
 		    int *ia, double *diag, double *b, int *jb, int
 		    *ib, int *ierr) ;  
-extern void coscal (int* nrow, int* job, int* nrm, double *a, int *ja,
+void coscal (int* nrow, int* job, int* nrm, double *a, int *ja,
 		    int *ia, double *diag, double *b, int *jb, int
 		    *ib, int *ierr) ;  
-extern int outputLU( iluptr lu, char *filename );
-extern int lumsolC(double *y, double *x, iluptr lu );
-extern void lumatvec(iluptr mat, double *x, double *y);
-extern int CSClum( int n, double *a, int *ja, int *ia, iluptr mat, int rsa ); 
-extern int CSClumC(csptr amat, iluptr mat, int rsa ); 
-extern void setup_arms (arms Levmat);
-extern int cleanARMS(arms ArmsPre);
-extern int csSplit4(csptr amat, int bsize, int csize, csptr B, csptr F,
+int outputLU( iluptr lu, char *filename );
+int lumsolC(double *y, double *x, iluptr lu );
+void lumatvec(iluptr mat, double *x, double *y);
+int CSClum( int n, double *a, int *ja, int *ia, iluptr mat, int rsa ); 
+int CSClumC(csptr amat, iluptr mat, int rsa ); 
+void setup_arms (arms Levmat);
+int cleanARMS(arms ArmsPre);
+int csSplit4(csptr amat, int bsize, int csize, csptr B, csptr F,
 		    csptr E, csptr C); 
 /* misc.c */
-extern int SparTran(csptr amat, csptr bmat, int job, int flag); 
-extern int coscalC(csptr mata, double *diag, int nrm);
-extern void dscale(int n, double *dd, double *x, double * y);
-extern void hilosort(csptr mat, int abval, int hilo);
-extern void printmat(FILE *ft, csptr A, int i0, int i1);
-extern void qqsort(int *ja, double *ma, int left, int right);
-extern void qsort2C(int *ja, double *ma, int left, int right, int
+int SparTran(csptr amat, csptr bmat, int job, int flag); 
+int coscalC(csptr mata, double *diag, int nrm);
+void dscale(int n, double *dd, double *x, double * y);
+void hilosort(csptr mat, int abval, int hilo);
+void printmat(FILE *ft, csptr A, int i0, int i1);
+void qqsort(int *ja, double *ma, int left, int right);
+void qsort2C(int *ja, double *ma, int left, int right, int
 		    abval); 
-extern void qsort3i(int *wa, int *cor1, int *cor2, int left, int
+void qsort3i(int *wa, int *cor1, int *cor2, int left, int
 		    right); 
-extern void qsortC(int *ja, double *ma, int left, int right, int
+void qsortC(int *ja, double *ma, int left, int right, int
 		   abval); 
-extern void qsortR2I(double *wa, int *cor1, int *cor2, int left, int
+void qsortR2I(double *wa, int *cor1, int *cor2, int left, int
 		     right); 
-extern int qsplitC(double *a, int *ind, int n, int ncut);
-extern int roscalC(csptr mata, double *diag, int nrm);
-extern void swapj(int v[], int i, int j);
-extern void swapm(double v[], int i, int j);
+int qsplitC(double *a, int *ind, int n, int ncut);
+int roscalC(csptr mata, double *diag, int nrm);
+void swapj(int v[], int i, int j);
+void swapm(double v[], int i, int j);
 
 /* piluNEW.c */
-extern int pilu(p4ptr amat, csptr B, csptr C, double *droptol, int
+int pilu(p4ptr amat, csptr B, csptr C, double *droptol, int
 		*lfil, csptr schur);
 
 /* ilutpC.c */
-extern int ilutD(csptr amat, double *droptol, int *lfil, ilutptr
+int ilutD(csptr amat, double *droptol, int *lfil, ilutptr
 		 ilusch);
-extern int ilutpC(csptr amat, double *droptol, int *lfil, double
+int ilutpC(csptr amat, double *droptol, int *lfil, double
 		  permtol, int mband, ilutptr ilusch);
 
 /* PQ.c */
-extern int PQperm(csptr mat, int bsize, int *Pord, int *Qord, int
+int PQperm(csptr mat, int bsize, int *Pord, int *Qord, int
 		  *nnod, double tol);
-extern int add2com(int *nback, int nod, int *iord, int *riord);
-extern int add2is(int *last, int nod, int *iord, int *riord);
-extern int indsetC(csptr mat, int bsize, int *iord, int *nnod, double
+int add2com(int *nback, int nod, int *iord, int *riord);
+int add2is(int *last, int nod, int *iord, int *riord);
+int indsetC(csptr mat, int bsize, int *iord, int *nnod, double
 		   tol); 
-extern int preSel(csptr mat, int *icor, int *jcor, int job, double
+int preSel(csptr mat, int *icor, int *jcor, int job, double
 		  tol, int *count);
 /* indsetC.c */
-extern int weightsC(csptr mat, double *w);
+int weightsC(csptr mat, double *w);
 
 /* setblks.c */
-extern int KeyComp( const void *vfst, const void *vsnd );
-extern int init_blocks( csptr csmat, int *pnBlock, int **pnB, int
+int KeyComp( const void *vfst, const void *vsnd );
+int init_blocks( csptr csmat, int *pnBlock, int **pnB, int
 			**pperm, double eps, double *t_hash, double
 			*t_angle );
 
 /* systimer.c */
-extern double sys_timer(void);
+double sys_timer(void);
 
 /*auxill.c */
 int read_inputs( char *in_file, io_t *pio );
