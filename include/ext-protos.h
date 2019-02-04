c459e18fe482336b60aec1d53734ef9540e53367
huiscliu <username@users.noreply.github.com>
2019-02-04 16:44:18 -0700

    format names

--- include/ext-protos.h
+++ include/ext-protos.h
@@ -14,7 +14,7 @@ extern "C" {
 #define max(a,b) (((a)>(b))?(a):(b))
 #endif
 
-#if defined(_IBM)
+#if defined(_IBM)  /* IBM */
 #define itsol_ddot(n,x,incx,y,incy)        FC_FUNC(ddot,DDOT)((n), (x), (incx), (y), (incy)) 
 #define itsol_dscal(n,alpha,x,incx)        FC_FUNC(dscal,DSCAL)((n), (alpha), (x), (incx)) 
 #define itsol_daxpy(n,alpha,x,incx,y,incy) FC_FUNC(daxpy,DAXPPY)((n), (alpha), (x), (incx), (y), (incy)) 
@@ -29,15 +29,20 @@ extern "C" {
   FC_FUNC(dgemm,DGEMM)((transa),(transb),						\
 	(l),(n),(m),(alpha),(a),					\
 	(lda),(b),(ldb),(beta),(c),(ldc))
+
 #define itsol_dgetrf(m, n, a, lda, ipvt, info)  \
   FC_FUNC(dgetrf,DGETRF)((m), (n), (a), (lda), (ipvt), (info))
+
 #define itsol_dgetri(n, a, lda, ipvt, work, lwork, info)		\
   FC_FUNC(dgetri,DGETRI)((n), (a), (lda), (ipvt), (work), (lwork), (info))
-#else
+
+#else  /* else */
+
 #define itsol_ddot(n,x,incx,y,incy)        FC_FUNC(ddot,DDOT)(&(n),(x),&(incx),(y),&(incy))
 #define itsol_dscal(n,alpha,x,incx)        FC_FUNC(dscal,DSCAL)(&(n),&(alpha),(x), &(incx))
 #define itsol_daxpy(n,alpha,x,incx,y,incy) FC_FUNC(daxpy,DAXPPY)(&(n), &(alpha), (x), &(incx), y, &(incy))
 #define itsol_dnrm2(n, x, incx)            FC_FUNC(dnrm2,DNRM2)(&(n), (x), &(incx))
+
 #define itsol_dgemv(transa, m, n, alpha, a, lda, x, incx, beta, y, incy)  \
   FC_FUNC(dgemv,DGEMV)((transa), &(m), &(n), &(alpha), (a), &(lda), (x), &(incx), \
 	 &(beta), (y), &(incy))
@@ -51,6 +56,8 @@ extern "C" {
 
 #define itsol_dgetri(n, a, lda, ipvt, work, lwork, info)			\
   FC_FUNC(dgetri,DGETRI)(&(n), (a), &(lda), (ipvt), (work), &(lwork), (info))
+
+#endif 
 
 /* FORTRAN routines */
 void FC_FUNC(itsol_readmtc,ITSOL_READMTC)(int*,  int*,  int*,  char*,  double*,  int*,
@@ -83,8 +90,6 @@ void FC_FUNC(dgetri,DGETRI)(int *n, double *a, int *lda, int *ipvt, double *work
 
 void FC_FUNC(itsol_gauss,ITSOL_GAUSS)(int *, double *, int *);
 void FC_FUNC(itsol_bxinv,ITSOL_BXINV)(int *, int *, double *, double *, double *);
-
-#endif 
 
 #ifdef __cplusplus
 }
