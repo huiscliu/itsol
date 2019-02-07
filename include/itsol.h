51842fab816d6b9b564702ba0f75edffac3ba4a7
huiscliu <username@users.noreply.github.com>
2019-02-07 11:29:28 -0700

    add assemble

--- include/itsol.h
+++ include/itsol.h
@@ -21,6 +21,8 @@ extern "C" {
 void itsol_solver_initialize(ITS_SOLVER *s, ITS_PC_TYPE pctype, ITS_SparMat *A);
 void itsol_solver_finalize(ITS_SOLVER *s);
 
+void itsol_solver_assemble(ITS_SOLVER *s);
+
 void itsol_pc_initialize(ITS_PC *pc, ITS_PC_TYPE pctype);
 void itsol_pc_finalize(ITS_PC *pc);
 
