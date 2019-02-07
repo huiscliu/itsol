b5ddb22808b08fa8cb710a2874e59af2ec2d3187
huiscliu <username@users.noreply.github.com>
2019-02-07 11:28:51 -0700

    add solver and pc

--- include/itsol.h
+++ include/itsol.h
@@ -14,4 +14,19 @@
 #include "pc-vbiluk.h"
 #include "pc-vbilut.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void itsol_solver_initialize(ITS_SOLVER *s, ITS_PC_TYPE pctype, ITS_SparMat *A);
+void itsol_solver_finalize(ITS_SOLVER *s);
+
+void itsol_pc_initialize(ITS_PC *pc, ITS_PC_TYPE pctype);
+void itsol_pc_finalize(ITS_PC *pc);
+
+double * itsol_solver_get_solution(ITS_SOLVER *s);
+
+#ifdef __cplusplus
+}
+#endif
 #endif 
