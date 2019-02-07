f71402539d4073420d2cd2e03fb873e5e3576481
huiscliu <username@users.noreply.github.com>
2019-02-07 11:39:01 -0700

    add solve

--- include/itsol.h
+++ include/itsol.h
@@ -23,6 +23,8 @@ void itsol_solver_finalize(ITS_SOLVER *s);
 
 void itsol_solver_assemble(ITS_SOLVER *s);
 
+void itsol_solver_solve(ITS_SOLVER *s, double *x, double *rhs);
+
 void itsol_pc_initialize(ITS_PC *pc, ITS_PC_TYPE pctype);
 void itsol_pc_finalize(ITS_PC *pc);
 void itsol_pc_assemble(ITS_PC *pc);
--- src/itsol.c
+++ src/itsol.c
@@ -5,6 +5,7 @@ void itsol_solver_initialize(ITS_SOLVER *s, ITS_PC_TYPE pctype, ITS_SparMat *A);
 void itsol_solver_finalize(ITS_SOLVER *s);
 
 void itsol_solver_assemble(ITS_SOLVER *s);
+void itsol_solver_solve(ITS_SOLVER *s, double *x, double *rhs);
 
 void itsol_pc_initialize(ITS_PC *pc, ITS_PC_TYPE pctype);
 void itsol_pc_finalize(ITS_PC *pc);
