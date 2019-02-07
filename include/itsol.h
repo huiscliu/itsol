5cfcdbd9f52b3e3075c7d8710031d40dda7c2283
huiscliu <username@users.noreply.github.com>
2019-02-07 11:35:53 -0700

    add assemble

--- include/itsol.h
+++ include/itsol.h
@@ -25,6 +25,7 @@ void itsol_solver_assemble(ITS_SOLVER *s);
 
 void itsol_pc_initialize(ITS_PC *pc, ITS_PC_TYPE pctype);
 void itsol_pc_finalize(ITS_PC *pc);
+void itsol_pc_assemble(ITS_PC *pc);
 
 double * itsol_solver_get_solution(ITS_SOLVER *s);
 
